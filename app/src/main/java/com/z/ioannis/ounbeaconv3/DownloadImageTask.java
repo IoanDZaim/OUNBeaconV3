d8cd811db6e8640e4f2a6c94e62638dc51502b73
IoanDZaim <ioanzaim@gmail.com>
2016-05-24 11:27:08 +0200

    minor changes

--- app/src/main/java/com/z/ioannis/ounbeaconv3/DownloadImageTask.java
+++ app/src/main/java/com/z/ioannis/ounbeaconv3/DownloadImageTask.java
@@ -38,15 +38,15 @@ public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
 
     protected Bitmap doInBackground(String... urls) {
         String urldisplay = urls[0];
-        Bitmap mIcon11 = null;
+        Bitmap dIcon = null;
         try {
             InputStream in = new java.net.URL(urldisplay).openStream();
-            mIcon11 = BitmapFactory.decodeStream(in);
+            dIcon = BitmapFactory.decodeStream(in);
         } catch (Exception e) {
             Log.e("Error", e.getMessage());
             e.printStackTrace();
         }
-        return mIcon11;
+        return dIcon;
     }
 
     protected void onPostExecute(Bitmap result) {
